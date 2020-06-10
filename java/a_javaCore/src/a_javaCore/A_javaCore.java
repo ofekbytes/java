5398926f4190ec3e0381548f99f341262e82b572
yaron <yaron.kessler41@gmail.com>
2020-06-10 10:16:09 +0300

    first Commit

---
+++ java/a_javaCore/src/a_javaCore/A_javaCore.java
@@ -0,0 +1,19 @@
+/**
+ * A_javaCore - main program
+ */
+package a_javaCore;
+
+public class A_javaCore {
+
+	public A_javaCore() {
+		
+		System.out.println("JavaCore is Active");
+
+	}
+
+	public static void main(String[] args) {
+		A_javaCore ajC = new A_javaCore();
+
+	}
+
+}
---
+++ java/am_JavaCore/src/main/java/am_JavaCore/Am_JavaCore.java
@@ -0,0 +1,14 @@
+package am_JavaCore;
+
+public class Am_JavaCore {
+
+	public Am_JavaCore() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
